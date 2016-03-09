0a78c9ec5d3fb8c0345f1cfe6c3478857988f78b
Emmanuel jayme <emanMac@emmanuels-macbook-pro.local>
2016-03-08 23:01:34 -0800

    Initial Commit

---
+++ badCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  badCalculator
+//
+//  Created by Emmanuel jayme on 8/3/16.
+//  Copyright © 2016 Emmanuel jayme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
