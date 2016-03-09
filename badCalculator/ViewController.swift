052790acb373c410e927bdfdec8381433d5aefda
ejaymejr <emanMac@Emmanuels-MacBook-Pro.local>
2016-03-09 18:30:30 +0800

    added files

--- badCalculator/ViewController.swift
+++ badCalculator/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var firstvalue = ""
+    
+    var operation = ""
+    
+    var currentvalue = ""
+    
+    @IBOutlet weak var display: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func numericTap(numButton : UIButton)
+    {
+        var numbers = display.text
+        if (numbers) == "0" || (numbers) == "+" {
+            display.text = (numButton.titleLabel!.text)
+        }else{
+            numbers = (display.text! + (numButton.titleLabel!.text)!)
+            display.text = (numbers)        }
+        
+    }
+    
+    @IBAction func plusTap(plusButton: UIButton) {
+        firstvalue = display.text!
+        display.text = "+"
+        operation = "+"
+    }
+    
+    @IBAction func equalsTap(eqButton: UIButton) {
+        currentvalue = display.text!
+        let num1 = Int(firstvalue)
+        let num2 = Int(currentvalue)
+        let total = num1! + num2!
+        display.text = String(total)
+    }
+    
+    
 }
 
